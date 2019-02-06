f0b5de9ed4ceba6b70d7a9e78d28b0e0ef7ff443
yuchnw <yuchn.w@gmail.com>
2019-02-06 15:31:30 -0600

    Updated algorithm, still testing

--- src/rrt.py
+++ src/rrt.py
@@ -1,14 +1,16 @@
 #!/usr/bin/env python
 
 import numpy as np
+import random
 import math
 import plotly
 import plotly.graph_objs as go
 import uuid
+from rtree import index
 
-X_RANGE = 3
-Y_RANGE = 3
-Z_RANGE = 3
+X_RANGE = 100
+Y_RANGE = 100
+Z_RANGE = 100
 
 class Node(object):
     
@@ -19,20 +21,19 @@ class Node(object):
 
 class RRT(object):
 
-    def _init_(self):
-        self.map = self.map[:,:,:].astype(int).T
-        self.q_init = Node(2,2,-2.5)
-        self.q_goal = Node(-2.5,2.5,2)
-        self.delta_q = 0.1
+    def __init__(self):
+        self.q_init = Node(0,99,0)
+        self.q_goal = Node(50,0,50)
+        self.delta_q = 10
         self.points = []
         self.parent = []
-        self.checkNum = 100
-        # p = index.Property()
-        # p.dimension = 3
-        # self.obs = index.Index(self.getObstacle(), interleaved=True, properties=p)
+        self.checkNum = 1
         self.Obstacles = np.array(
         [(20, 20, 20, 40, 40, 40), (20, 20, 60, 40, 40, 80), (20, 60, 20, 40, 80, 40), (60, 60, 20, 80, 80, 40),
         (60, 20, 20, 80, 40, 40), (60, 20, 60, 80, 40, 80), (20, 60, 60, 40, 80, 80), (60, 60, 60, 80, 80, 80)])
+        p = index.Property()
+        p.dimension = 3
+        self.obs = index.Index(self.getObstacle(), interleaved=True, properties=p)
         self.points.append(self.q_init)
         self.parent.append(self.q_init)
     
@@ -46,15 +47,17 @@ class RRT(object):
         return near
 
     def getNew(self,rand,near):
-        scale = self.delta_q/math.sqrt(self.getDistance(rand,near))
+        scale = float(self.delta_q/math.sqrt(self.getDistance(rand,near)))
         new_X = near.x + scale*(rand.x-near.x)
         new_Y = near.y + scale*(rand.y-near.y)
         new_Z = near.z + scale*(rand.z-near.z)
+        print(scale)
         new = self.check_boundary(Node(new_X,new_Y,new_Z))
         return new
 
     def check_obstacle(self,point):
-        if self.map[point.x,point.y,point.z] == 0:
+        #True if not inside of obstacle, false otherwise
+        if self.obs.count((point.x,point.y,point.z)) == 0:
             return True
         else:
             return False
@@ -73,33 +76,38 @@ class RRT(object):
         return point
 
     def check_line(self,p1,p2):
-        dx = (p2.x-p1.x)/self.checkNum
-        dy = (p2.y-p1.y)/self.checkNum
-        dz = (p2.z-p1.z)/self.checkNum
-        for i in range(self.checkNum):
-            temp = Node(dx+i*p1.x,dy+i*p2.y,dz+i*p1.z)
-            if self.check_obstacle(temp):
+        count = math.sqrt(self.getDistance(p1,p2))/self.checkNum
+        dx = (p2.x-p1.x)/count
+        dy = (p2.y-p1.y)/count
+        dz = (p2.z-p1.z)/count
+        for i in range(int(count)):
+            temp = Node(i*dx+p1.x,i*dy+p1.y,i*dz+p1.z)
+            if self.check_obstacle(temp) == False:
                 # current line segment is within obstacle
                 return False
         return True
 
     def check_done(self,point):
-        if self.check_line(point,self.q_goal):
+        if self.check_line(point,self.q_goal) == True:
             self.points.append(self.q_goal)
             self.parent.append(point)
             return True
-        return False
+        else:
+            return False
 
     def getObstacle(self):
         for obs in self.Obstacles:
             yield (uuid.uuid4(), obs, obs)
 
+    def getMap(self,obs):
+        return
+
     def plot(self):
         plotly.tools.set_credentials_file(username='yuchnw', api_key='U4zDLjU3ftIprgEzyXTz')
         trace1 = go.Scatter3d(
-            x=[0,100],
-            y=[0,100],
-            z=[0,100],
+            x=[0,50],
+            y=[99,0],
+            z=[0,50],
             mode='markers',
             marker=dict(
                 size=20,
@@ -123,10 +131,9 @@ class RRT(object):
                 j=[3, 4, 1, 2, 5, 6, 5, 2, 0, 1, 6, 3],
                 k=[0, 7, 2, 3, 6, 7, 1, 1, 5, 5, 7, 6],
                 color='#FFB6C1',
-                opacity=0.70
+                opacity=1
             )
             data.append(obs)
-        # data = [trace2,trace3]
         layout = go.Layout(
             margin=dict(
                 l=0,
@@ -139,21 +146,25 @@ class RRT(object):
         plotly.plotly.plot(fig, filename='3d_cube',auto_open=True)
 
     def main(self):
-        
         while True:
-            q_rand = Node(np.random.randint(-X_RANGE,X_RANGE),np.random.randint(-Y_RANGE,Y_RANGE),np.random.randint(-Z_RANGE,Z_RANGE))
+            q_rand = Node(np.random.uniform(0,X_RANGE),np.random.uniform(0,Y_RANGE),np.random.uniform(0,Z_RANGE))
             q_near = self.getNearest(q_rand)
+            # print(q_near.x,q_near.y,q_near.z)
             q_new = self.getNew(q_rand,q_near)
-            if self.check_done(q_new):
-                break
+            # print(q_new.x,q_new.y,q_new.z)
+            if 0.1 and random.random() < 0.1:
+                if self.check_done(q_new):
+                    print(len(self.points))
+                    return self.points 
             if not self.check_line(q_near,q_new):
                 continue
             if self.check_obstacle(q_new):
                 continue
             self.points.append(q_new)
             self.parent.append(q_near)
+        print(len(self.points))
+        # self.plot()
 
 if __name__=="__main__":
-    # N_map = plt.imread("images/N_map.png")
     rrt = RRT()
-    rrt.plot()
+    rrt.main()
