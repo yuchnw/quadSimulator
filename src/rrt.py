6a2bc8ddee6974d4c08625ba08cdc7c51acbf770
yuchnw <yuchn.w@gmail.com>
2019-02-05 21:19:36 -0600

    Modify obstacles

--- src/rrt.py
+++ src/rrt.py
@@ -4,6 +4,7 @@ import numpy as np
 import math
 import plotly
 import plotly.graph_objs as go
+import uuid
 
 X_RANGE = 3
 Y_RANGE = 3
@@ -26,7 +27,12 @@ class RRT(object):
         self.points = []
         self.parent = []
         self.checkNum = 100
-
+        # p = index.Property()
+        # p.dimension = 3
+        # self.obs = index.Index(self.getObstacle(), interleaved=True, properties=p)
+        self.Obstacles = np.array(
+        [(20, 20, 20, 40, 40, 40), (20, 20, 60, 40, 40, 80), (20, 60, 20, 40, 80, 40), (60, 60, 20, 80, 80, 40),
+        (60, 20, 20, 80, 40, 40), (60, 20, 60, 80, 40, 80), (20, 60, 60, 40, 80, 80), (60, 60, 60, 80, 80, 80)])
         self.points.append(self.q_init)
         self.parent.append(self.q_init)
     
@@ -84,24 +90,43 @@ class RRT(object):
             return True
         return False
 
+    def getObstacle(self):
+        for obs in self.Obstacles:
+            yield (uuid.uuid4(), obs, obs)
+
     def plot(self):
         plotly.tools.set_credentials_file(username='yuchnw', api_key='U4zDLjU3ftIprgEzyXTz')
-        x, y, z = np.random.multivariate_normal(np.array([0,0,0]), np.eye(3), 100).transpose()
         trace1 = go.Scatter3d(
-            x=x,
-            y=y,
-            z=z,
+            x=[0,100],
+            y=[0,100],
+            z=[0,100],
             mode='markers',
             marker=dict(
                 size=20,
                 line=dict(
                     color='rgba(217, 217, 217, 0.14)',
-                    width=0.5
+                    width=10
                 ),
                 opacity=0.8
             )
         )
         data = [trace1]
+        Obstacles = np.array(
+        [(20, 20, 20, 40, 40, 40), (20, 20, 60, 40, 40, 80), (20, 60, 20, 40, 80, 40), (60, 60, 20, 80, 80, 40),
+        (60, 20, 20, 80, 40, 40), (60, 20, 60, 80, 40, 80), (20, 60, 60, 40, 80, 80), (60, 60, 60, 80, 80, 80)])
+        for O_i in Obstacles:
+            obs = go.Mesh3d(
+                x=[O_i[0], O_i[0], O_i[3], O_i[3], O_i[0], O_i[0], O_i[3], O_i[3]],
+                y=[O_i[1], O_i[4], O_i[4], O_i[1], O_i[1], O_i[4], O_i[4], O_i[1]],
+                z=[O_i[2], O_i[2], O_i[2], O_i[2], O_i[5], O_i[5], O_i[5], O_i[5]],
+                i=[7, 0, 0, 0, 4, 4, 6, 6, 4, 0, 3, 2],
+                j=[3, 4, 1, 2, 5, 6, 5, 2, 0, 1, 6, 3],
+                k=[0, 7, 2, 3, 6, 7, 1, 1, 5, 5, 7, 6],
+                color='#FFB6C1',
+                opacity=0.70
+            )
+            data.append(obs)
+        # data = [trace2,trace3]
         layout = go.Layout(
             margin=dict(
                 l=0,
@@ -111,9 +136,10 @@ class RRT(object):
             )
         )
         fig = go.Figure(data=data, layout=layout)
-        plotly.plotly.plot(fig, filename='3d_obstacle',auto_open=True)
+        plotly.plotly.plot(fig, filename='3d_cube',auto_open=True)
 
     def main(self):
+        
         while True:
             q_rand = Node(np.random.randint(-X_RANGE,X_RANGE),np.random.randint(-Y_RANGE,Y_RANGE),np.random.randint(-Z_RANGE,Z_RANGE))
             q_near = self.getNearest(q_rand)
