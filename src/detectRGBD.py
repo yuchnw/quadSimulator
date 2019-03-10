0a9c637440c3ec61f6f5e9dd4a492507caffecb9
yuchnw <yuchn.w@gmail.com>
2019-03-09 20:37:38 -0600

    Add depth array value (noisy)

--- src/detectRGBD.py
+++ src/detectRGBD.py
@@ -7,6 +7,7 @@ from geometry_msgs.msg import PoseStamped
 import cv2
 from sensor_msgs.msg import CameraInfo
 from sensor_msgs.msg import Image
+import message_filters
 
 class DetectHand:
 
@@ -119,63 +120,89 @@ class DetectHand:
             else:
                 return None
         
+    # def imageCallback(self,image):
+    #     from cv_bridge import CvBridge, CvBridgeError
+    #     bridge = CvBridge()
+    #     cv_image = bridge.imgmsg_to_cv2(image, desired_encoding="bgr8")
+    #     (rows,cols,channels) = cv_image.shape
+    #     self.operate(cv_image)
+    #     return
+
+    def depthCallback(self,image):
+        from cv_bridge import CvBridge, CvBridgeError
+        bridge = CvBridge()
+        depth_image = bridge.imgmsg_to_cv2(image, desired_encoding="32FC1")
+        # z = depth_image.distance
+        # print(z)
+        # depth_array = np.array(depth_image, dtype=np.float32)
+        # cv2.normalize(depth_array, depth_array, 0, 1, cv2.NORM_MINMAX)
+        # gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+    def getDepth(self,rgb,depth):
+        from cv_bridge import CvBridge, CvBridgeError
+        bridge = CvBridge()
+        try:
+            image = bridge.imgmsg_to_cv2(rgb, "bgr8")
+            depth_image = bridge.imgmsg_to_cv2(depth, "32FC1")
+        except CvBridgeError, e:
+            print e
+        depth_array = np.array(depth_image, dtype=np.float32)
+        # cv2.normalize(depth_array, depth_array, 0, 1, cv2.NORM_MINMAX)
+        # print(depth_array[20][319])
+        self.operate(image,depth_array)
+        # cv2.imshow('Body Recognition', depth_array)
+        cv2.waitKey(3)
+        return
 
     def main(self):
-        hand_publisher = rospy.Publisher('/iris/hand', PoseStamped, queue_size=10)
-        # loop_rate = rospy.Rate(30)
+    #     rospy.Subscriber("/camera/rgb/image_color", Image, self.imageCallback)
+    #     rospy.Subscriber("/camera/depth/image_raw", Image, self.depthCallback)
+        image_sub = message_filters.Subscriber("/camera/rgb/image_color", Image)
+        depth_sub = message_filters.Subscriber("/camera/depth/image_raw", Image)
+        both = message_filters.ApproximateTimeSynchronizer([image_sub, depth_sub], 10, 0.5)
+        both.registerCallback(self.getDepth)
+        rospy.spin()
 
+    def operate(self,cv_image,depth_array):
+        hand_publisher = rospy.Publisher('/iris/hand', PoseStamped, queue_size=10)
         handPos = PoseStamped()
-        # capture = cv2.VideoCapture('/dev/video2')
-        capture = cv2.VideoCapture(0)
-        height = capture.get(cv2.CAP_PROP_FRAME_HEIGHT)
-        width = capture.get(cv2.CAP_PROP_FRAME_WIDTH)
-        loop_rate = rospy.Rate(30)
-        # while not rospy.is_shutdown():
-        while(capture.isOpened()):
-            ret, frame_raw = capture.read()
-            while not ret:
-                ret,frame_raw = capture.read()
-            frame_raw = cv2.flip(frame_raw,1)
-            frame = frame_raw[:int(height),:int(width)]
-            if cv2.waitKey(1) == ord('z') & 0xFF:
-                self.handHistogram(frame)
-            if cv2.waitKey(1) == ord('a') & 0xFF:
-                self.ok = True
-            if self.set_hist == True:
-                frame = self.histogramMask(frame,self.hand_hist)
-                contour = self.maxContour(frame)
-                farthest = self.getCentroidandFingertip(contour,frame)
-                if self.ok == True:
-                    if self.start == False:
+        loop_rate = rospy.Rate(50)
+        if cv2.waitKey(1) == ord('z') & 0xFF:
+            self.handHistogram(cv_image)
+        if cv2.waitKey(1) == ord('a') & 0xFF:
+            self.ok = True
+        if self.set_hist == True:
+            cv_image = self.histogramMask(cv_image,self.hand_hist)
+            contour = self.maxContour(cv_image)
+            farthest = self.getCentroidandFingertip(contour,cv_image)
+            if self.ok == True:
+                if self.start == False:
+                    self.path.append(farthest)
+                    self.start = True
+                else:
+                    dx = self.path[-1][0] - farthest[0]
+                    dy = self.path[-1][1] - farthest[1]
+                    dist = np.sqrt(dx*dx + dy*dy)
+                    if dist > 10 and dist < 50:
+                        # print(farthest[1])
+                        print(depth_array[farthest[1]][farthest[0]])
                         self.path.append(farthest)
-                        self.start = True
-                    else:
-                        dx = self.path[-1][0] - farthest[0]
-                        dy = self.path[-1][1] - farthest[1]
-                        dist = np.sqrt(dx*dx + dy*dy)
-                        if dist > 10 and dist < 50:
-                            self.path.append(farthest)
-                            handPos.pose.position.x = farthest[0]/640.0*100 -50
-                            handPos.pose.position.y = 50 - farthest[1]/480.0*100
-                            handPos.pose.position.z = 10
-                    if len(self.path) > 20:
-                        self.path.pop(0)
-                    if self.path is not None:
-                        for i in range(len(self.path)-1):
-                            cv2.circle(frame, self.path[i], 4, [0, 0, 255], -1)
-            if self.ok == False:
-                frame = self.drawRectangle(frame)
-            
-            # frame = np.vstack([hand_frame, frame])
-            hand_publisher.publish(handPos)
-            cv2.imshow('frame',frame)
-            if cv2.waitKey(1) & 0xFF == ord('q'):
-                print("stop")
-                break
-            loop_rate.sleep()
-        capture.release()
-        cv2.destroyAllWindows()
+                        handPos.pose.position.x = farthest[0]/640.0*100 -50
+                        handPos.pose.position.y = 50 - farthest[1]/480.0*100
+                        handPos.pose.position.z = 10
+                if len(self.path) > 20:
+                    self.path.pop(0)
+                if self.path is not None:
+                    for i in range(len(self.path)-1):
+                        cv2.circle(cv_image, self.path[i], 4, [0, 0, 255], -1)
+        if self.ok == False:
+            cv_image = self.drawRectangle(cv_image)
+        hand_publisher.publish(handPos)
+        cv2.imshow('frame',cv_image)
+        loop_rate.sleep()
+        return
 
 if __name__ == '__main__':
     rospy.init_node('detectHand_node',anonymous=True)
+    
     DetectHand().main()
